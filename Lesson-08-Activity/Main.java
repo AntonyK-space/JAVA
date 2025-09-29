4c4e2ccab5d721010cad7a068a150586d8f48cc3
AntonyK-space <antonyk7@nycstudents.net>
2025-09-29 14:11:40 +0000

    u

--- Lesson-08-Activity/Main.java
+++ Lesson-08-Activity/Main.java
@@ -4,20 +4,24 @@ class Main {
     	(new Main()).init();
 	}
 
-  double takeF( double f ){
-    return (f - 32.0) * (5.0/9.0);
+  takeF( double f ){
+    double FtoC = (f - 32.0) * (5.0/9.0);
+	return FtoC;
   }
 
-  double rad( double rad ){
-    return (4/3) * 3.14 * Math.pow(rad, 3);
+  rad( double rad ){
+    double SphereVol = (4/3) * 3.14 * Math.pow(rad, 3);
+	return SphereVol;
   }
 
-  double radheight( double radi, double height ){
-    return 3.14 * Math.pow(radi, 2) * (height/3.0);
+  radheight( double radi, double height ){
+    double ConeVol = 3.14 * Math.pow(radi, 2) * (height/3.0);
+	return ConeVol;
   }
 
-  double x1x2y1y2( double x1, double x2, double y1, double y2 ){
-    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+  x1x2y1y2( double x1, double x2, double y1, double y2 ){
+    double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+	return distance;
   }
 
 
