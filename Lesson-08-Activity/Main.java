5912a2fe9d81d0887cc1abe69070cc2f25e291c7
AntonyK-space <antonyk7@nycstudents.net>
2025-09-30 14:09:56 +0000

    mmmmmmmmm

--- Lesson-08-Activity/Main.java
+++ Lesson-08-Activity/Main.java
@@ -10,7 +10,7 @@ class Main {
   }
 
   double rad( double rad ){
-    double SphereVol = (4/3) * 3.14 * Math.pow(rad, 3);
+    double SphereVol = (4/3) * Math.PI * Math.pow(rad, 3);
 	return SphereVol;
   }
 
@@ -25,8 +25,11 @@ class Main {
   }
 
   void init(){
+    print( FtoC );
     takeF( 5.0 );
     rad( 50.4 );
+    radiheight( 25.6, 75.9 );
+    x1x2y1y2( 6.6, 3.2, 6.2, 9.5 );
   }
  
 }
