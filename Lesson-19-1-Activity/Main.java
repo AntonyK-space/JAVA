559683874d8bbbbc4ac7eafa2e2c7e690d5b3e59
AntonyK-space <antonyk7@nycstudents.net>
2025-11-07 14:59:51 +0000

    gamble

--- Lesson-19-1-Activity/Main.java
+++ Lesson-19-1-Activity/Main.java
@@ -10,17 +10,27 @@ class Main {
   void init(){
       String x = rollDice();
       System.out.println(x);
-
+      String y = lotto();
+      System.out.println(y);
     
   }
 
   String rollDice(){
-    String bld ="";
+    String bld = "";
+    String space = " ";
     for(int x = 1; x <= 2; x++){
-      bld += (int)(Math.random() * 6) + 1 ;
+      bld += (int)(Math.random() * 7) + 1 + space;
     }
     return bld;
   }
 
+  String lotto(){
+    String bld = "";
+    String space = " ";
+    for(int x = 1; x <= 5; x++){
+      bld += (int)(Math.random() * 49) + 1 + space;
+    }
+    return bld;
+  }
 
 }
