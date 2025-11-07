2443ef9414a5d3581e25e01ccd68b9980efcd2f4
AntonyK-space <antonyk7@nycstudents.net>
2025-11-07 15:10:47 +0000

    jnifewijnfewjinfewnjewfo

--- Lesson-19-1-Activity/Main.java
+++ Lesson-19-1-Activity/Main.java
@@ -19,7 +19,7 @@ class Main {
     String bld = "";
     String space = " ";
     for(int x = 1; x <= 2; x++){
-      bld += (int)(Math.random() * 7) + 1 + space;
+      bld += (int)(Math.random() * 6) + 1 + space;
     }
     return bld;
   }
@@ -28,7 +28,25 @@ class Main {
     String bld = "";
     String space = " ";
     for(int x = 1; x <= 5; x++){
-      bld += (int)(Math.random() * 49) + 1 + space;
+      bld += (int)(Math.random() * 48) + 1 + space;
+    }
+    return bld;
+  }
+
+  String additionTutor(String playeranswer){
+    int firstnum = "";
+    int secondnum = "";
+    for(int x = 1; x <= 1; x++){
+      firstnum = (int)(Math.random() * 999999999) + 1 + space;
+      secondnum = (int)(Math.random() * 999999999) + 1 + space;
+      String bld = firstnum + secondnum;
+      System.out.println(bld);
+      if(playeranswer.equals){
+        return "correct";
+      }
+      else{
+        return "wrong";
+      }
     }
     return bld;
   }
