23e73c5c14194e775fd0e04ff17aeffef9c3750c
AntonyK-space <antonyk7@nycstudents.net>
2025-12-08 14:39:24 +0000

    bluu

--- Random-Code/Main.java
+++ Random-Code/Main.java
@@ -6,43 +6,43 @@ class Main {
   void printt(Object o){ System.out.print(o);}
 
   void init(){
-        String x = sentence(javac);
-        System.out.println(x);
-        String z = encode("word");
-        System.out.println(z);
+        //String x = sentence(javac);
+        //System.out.println(x);
+        //String z = encode("word");
+        //System.out.println(z);
   }
 
-  String sentence(String msg){
-  int count=0;
-  String bld = "";
-  for(int x=0; x<=msg.length()-1; x++){
-    bld = 
-    if(msg.indexOf(c).equals('c')){
-   	count++;
-    }
-  }
-  return count;
-}
+  //String sentence(String msg){
+  //int count=0;
+  //String bld = "";
+  //for(int x=0; x<=msg.length()-1; x++){
+    //bld = 
+    //if(msg.indexOf(c).equals('c')){
+   	//count++;
+   // }
+ // }
+  //return count;
+//}
   
 
 
-String encode(String s){
-  String bld = "";
-  for(int i = 0; i < s.length; i++){
-    char ch = s.charAt(i);
-    if(ch == 'z'){
-      bld += 'a';
-    }
-    else if(ch == 'Z'){
-      bld += 'Z';
-    }
-    else{
-      int ascii = (int) ch;
-      ch = (char) (ascii+1);
-      bld += ch;
-    }
-  }
-  return bld;
-}
+//String encode(String s){
+//  String bld = "";
+//  for(int i = 0; i < s.length; i++){
+//    char ch = s.charAt(i);
+//    if(ch == 'z'){
+//      bld += 'a';
+//    }
+//    else if(ch == 'Z'){
+//      bld += 'Z';
+//    }
+//    else{
+//      int ascii = (int) ch;
+//      ch = (char) (ascii+1);
+//      bld += ch;
+//    }
+//  }
+//  return bld;
+// }
 
 }
--- Random-Code/Main0.java
+++ Random-Code/Main0.java
@@ -6,9 +6,25 @@ class Main {
   void printt(Object o){ System.out.print(o);}
 
   void init(){
-    
+    //Input.writeFile("Encoded.txt", );
+    String rome = Input.readFile("Original.txt");
+    String encoded = encode(rome);
+    System.out.println(encoded);
   }
 
+  String encode(String x){
+    String bld = "";
+    char ch = ' ';
+    for(int i = 0; i < x.length(); i++){
+       ch = x.charAt(i);
+      if(x.length() != 100){
+        int ascii = (int) ch;
+        ch = (char) (ascii - 3);
+        bld += ch;
+      }
+    }
+    return bld;
+  }
 
 
 
