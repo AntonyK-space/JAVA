e03b437e4b86cc7fa461cca70868acb916eac37a
AntonyK-space <antonyk7@nycstudents.net>
2025-12-15 15:12:54 +0000

    project mmmmmmmmmmmmmm

--- Lesson-27-demosample/Main.java
+++ Lesson-27-demosample/Main.java
@@ -6,22 +6,19 @@ class Main {
   void printt(Object o){ System.out.print(o);}
 
   void init(){
-
-    // This example we are substituting all lower case 
-    // letters to another lower case letter.
     char[] sub = new char[5];
     sub[0] = 'a';
     sub[1] = 'e';
     sub[2] = 'i';
-    sub[3] = 'o';
-    sub[4] = 'u';
+    sub[3] = 'l';
+    sub[4] = 'o';
 
     char[] sub2 = new char[5];
-    sub2[0] = '\u2663';  // Club
-    sub2[1] = '\u2660';  // Spade
-    sub2[2] = '\u2665';  // Heart
-    sub2[3] = '\u2666';  // Diamond
-    sub2[4] = '\u2836';  // Bralle symbol
+    sub2[0] = '4';  // Club
+    sub2[1] = '3';  // Spade
+    sub2[2] = '1';  // Heart
+    sub2[3] = '1';  // Diamond
+    sub2[4] = '0';  // Bralle symbol
 
     
     // Encoding message
@@ -29,11 +26,11 @@ class Main {
 
     //substituion
     String encodedMsg1 = subEncryption(file,sub,sub2);
-    //Input.writeFile("Encode1.txt",encodedMsg1);
+    Input.writeFile("Encode1.txt",encodedMsg1);
 
     // caesar cipher
     String encodedMsg2 = encode(encodedMsg1);
-    //Input.writeFile("Encode2.txt",encodedMsg2);
+    Input.writeFile("Encode2.txt",encodedMsg2);
 
     // reverse
     String encodedMsg3 = reverse(encodedMsg2);
@@ -41,23 +38,26 @@ class Main {
 
     
     // decoding message
-    String file2 = Input.readFile("Encode1.txt");
+    //String file2 = Input.readFile("Encode1.txt");
     
-    String decodedMsg1 = reverse(file2);
+    //String decodedMsg1 = reverse(file2);
     //Input.writeFile("Decode1.txt", decodedMsg1);
     
-    String decodedMsg2 = decode(decodedMsg1);
+    //String decodedMsg2 = decode(decodedMsg1);
     //Input.writeFile("Decode2.txt", decodedMsg2);
     
-     String decodedMsg3 = subEncryption(decodedMsg2, sub2, sub);
+     //String decodedMsg3 = subEncryption(decodedMsg2, sub2, sub);
     //Input.writeFile("Decode1.txt", decodedMsg3);
-    
+  
+    String encoded = reverse(file);
+    String newcode = encode(encoded);
+    System.out.println(newcode);
     
   }
   // Level 1 reverse string
   String reverse(String txt){
-    String bld ="";
-    for(int i = txt.length; i > 0; i--){
+    String bld = "";
+    for(int i = txt.length(); i > 0; i--){
       bld += txt.substring(i, i + 1);
     }
     return bld;
@@ -65,10 +65,16 @@ class Main {
   
   
   //Level 2 Cipher encoding with no wrapping
-  String encode(String txt){
-    String bld="";
-    
-     
+  String encode(String x){
+    String bld= "";
+    for(int i = 0; i < x.length(); i++){
+      char ch = x.charAt(i);
+      if(x.length() != 100){
+        int ascii = (int) ch;
+        ch = (char) (ascii - 3);
+        bld += ch;
+      }
+    }
     return bld;
   }
 
--- Random-Code/Main.java
+++ Random-Code/Main.java
@@ -6,43 +6,192 @@ class Main {
   void printt(Object o){ System.out.print(o);}
 
   void init(){
-        //String x = sentence(javac);
-        //System.out.println(x);
-        //String z = encode("word");
-        //System.out.println(z);
+    String rome = Input.readFile("Original.txt");
+    String encoded = encode(rome);
+    Input.writeFile("Encode1.txt",encoded);
+    String sw = Input.readFile("Encode1.txt");
+    String swap = paws(sw);
+    Input.writeFile("Encode2.txt", swap);
+    String sub = Input.readFile("Encode2.txt");
+    String substitution = geometry(sub);
+    Input.writeFile("Encode3.txt", substitution);
   }
 
-  //String sentence(String msg){
-  //int count=0;
-  //String bld = "";
-  //for(int x=0; x<=msg.length()-1; x++){
-    //bld = 
-    //if(msg.indexOf(c).equals('c')){
-   	//count++;
-   // }
- // }
-  //return count;
-//}
-  
+  String encode(String x){
+    String bld = "";
+    char ch = ' ';
+    for(int i = 0; i < x.length(); i++){
+       ch = x.charAt(i);
+      if( ch == 'v'){
+        bld += 'a';
+      }
+      else if( ch == 'W'){
+        bld += 'B';
+      }
+      else if( ch == 'w'){
+        bld += 'b';
+      }
+      else if( ch == 'X'){
+        bld += 'c';
+      }
+      else if( ch == 'x'){
+        bld += 'c';
+      }
+      else if( ch == 'Y'){
+        bld += 'D';
+      }
+      else if( ch == 'y'){
+        bld += 'd';
+      }
+      else if( ch == 'Z'){
+        bld += 'e';
+      }
+      else if( ch == 'z'){
+        bld += 'e';
+      }
+      else if( ch == 'V'){
+        bld += 'A';
+      }
+      else if( ch == ' '){
+        bld += ' ';
+      }
+      else if( ch == '.'){
+        bld += '.';
+      }
+      else if( ch == ','){
+        bld += ',';
+      }
+      else if( ch == '1'){
+        bld += '6';
+      }
+      else if( ch == '2'){
+        bld += '7';
+      }
+      else if( ch == '3'){
+        bld += '8';
+      }
+      else if( ch == '4'){
+        bld += '9';
+      }
+      else if( ch == '5'){
+        bld += '0';
+      }
+      else if( ch == '6'){
+        bld += '1';
+      }
+      else if( ch == '7'){
+        bld += '2';
+      }
+      else if( ch == '8'){
+        bld += '3';
+      }
+      else if( ch == '9'){
+        bld += '4';
+      }
+      else if( ch == '0'){
+        bld += '5';
+      }
+      else if(x.length() != 999999999){
+        int ascii = (int) ch;
+        ch = (char) (ascii + 5);
+        bld += ch;
+      }
+    }
+    return bld;
+  }
+
+    String paws(String x){
+      String bld = "";
+      char ch = ' ';
+      int placement = 0;
+      for(int i = 0; i < x.length(); i++){
+        placement = i % 2;
+        ch = x.charAt(i);
+        int ascii = (int) ch;
+        if( ch == ' '){
+          bld += ' ';
+        }
+        else if( ch == '.'){
+          bld += '.';
+        }
+        else if( ch == ','){
+          bld += ',';
+        }
+        else if( ch == 'Z' && placement == 0){
+          bld += 'A';
+        }
+        else if( ch == 'z' && placement == 0){
+          bld += 'a';
+        }
+      else if( ch == '1' && placement == 0){
+        bld += '2';
+      }
+      else if( ch == '2' && placement == 0){
+        bld += '3';
+      }
+      else if( ch == '3' && placement == 0){
+        bld += '4';
+      }
+      else if( ch == '4' && placement == 0){
+        bld += '5';
+      }
+      else if( ch == '5' && placement == 0){
+        bld += '6';
+      }
+      else if( ch == '6' && placement == 0){
+        bld += '7';
+      }
+      else if( ch == '7' && placement == 0){
+        bld += '8';
+      }
+      else if( ch == '8' && placement == 0){
+        bld += '9';
+      }
+      else if( ch == '9' && placement == 0){
+        bld += '0';
+      }
+      else if( ch == '0' && placement == 0){
+        bld += '1';
+      }
+        else if( placement == 0){;
+          ch = (char) (ascii + 1);
+          bld += ch;
+        }
+        else{
+          bld += ch;
+        }
+      }
+      return bld;
+    }
+
+    String geometry(String t){
+      String bld = "";
+      char ch = ' ';
+      for(int i = 0; i < t.length(); i++){
+       ch = t.charAt(i);
+       int ascii = (int) ch;
+      if( ascii >= 97 && ascii <= 122){
+       int newascii = ascii - 33;
+       ch = (char) (newascii + 9632);
+       bld += ch;
+      }
+      else if( ascii >= 65 && ascii <= 90){
+       int newAscii = ascii - 65;
+       ch = (char) (newAscii + 9632);
+       bld += ch;
+      }
+      else if( ch == ' '){
+        bld += ' ';
+      }
+      else if( ch == '.'){
+        bld += '.';
+      }
+      else if( ch == ','){
+        bld += ',';
+      }
+    }
+    return bld;
+  }
 
 
-//String encode(String s){
-//  String bld = "";
-//  for(int i = 0; i < s.length; i++){
-//    char ch = s.charAt(i);
-//    if(ch == 'z'){
-//      bld += 'a';
-//    }
-//    else if(ch == 'Z'){
-//      bld += 'Z';
-//    }
-//    else{
-//      int ascii = (int) ch;
-//      ch = (char) (ascii+1);
-//      bld += ch;
-//    }
-//  }
-//  return bld;
-// }
-
-}
+  }
